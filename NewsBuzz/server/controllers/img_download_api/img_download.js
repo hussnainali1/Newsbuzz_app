e57b717c5de39b544cd7acb6d3bc535cb5e83f9b
hussnain_ali <33692524+hussnainali1@users.noreply.github.com>
2020-06-09 14:46:47 +0500

    commit

--- NewsBuzz/server/controllers/img_download_api/img_download.js
+++ NewsBuzz/server/controllers/img_download_api/img_download.js
@@ -6,54 +6,5 @@ var fs = require("fs");
 var request = require("request");
 
 exports.readJobs = async (req, res) => {
-  try {
-    const jobs_R = await jobs_news
-      .find()
-
-
-    let i = 0;
-
-
-    while (jobs_R[i] != null) {
-      // -----------------------------------------------
-
-      console.log("===" + i + "====");
-      console.log("this is id without conversion == " + jobs_R[i]._id);
-      // console.log("this is id with conversion == " + jobs_R._id[i].toHaxString());
-      // -----------------------------------------------
-
-
-      img_name = jobs_R[i]._id + ".jpg";
-
-      console.log(img_name);
-      img_link = jobs_R[i].description_img_link;
-      // ====================================
-
-      var download = function (uri, filename, callback) {
-        request.head(uri, function (err, res, body) {
-          console.log("content-type:", res.headers["content-type"]);
-          console.log("content-length:", res.headers["content-length"]);
-
-          request(uri)
-            .pipe(
-              fs.createWriteStream(
-                "./img_to_be_OCR//" +
-                filename
-              )
-            )
-            .on("close", callback);
-        });
-      };
-
-      download(img_link, img_name, function () {
-        console.log("done");
-      });
-      // ===================================
-      i = i + 1;
-    } return res.json({ message: " all images are downloaded" });
-  } catch (err) {
-    console.error(err.message);
-    return res.status(500).send("Server error");
-  }
 
 };
